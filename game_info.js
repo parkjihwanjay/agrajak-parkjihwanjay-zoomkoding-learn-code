5ec6244b8fffb2d614e4180a488223390a6755b4
woowahan12 <woowahan12@woowahan12ui-MacBookPro.local>
2020-07-02 16:06:23 +0900

    Create game info class #3

---
+++ game_info.js
@@ -0,0 +1,46 @@
+class GameInfo {
+    constructor(canvas, ctx, brickCount) {
+        this.score = 0;
+        this.lives = 3;
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.brickCount = brickCount;
+    }
+
+    drawScore() {
+        this.ctx.font = "16px Arial";
+        this.ctx.fillStyle = "#0095DD";
+        this.ctx.fillText("Score: "+this.score, 8, 20);
+    }
+    drawLives() {
+        this.ctx.font = "16px Arial";
+        this.ctx.fillStyle = "#0095DD";
+        this.ctx.fillText("Lives: "+this.lives, this.canvas.width-65, 20);
+    }
+    
+    isWon() {
+        return this.score == this.brickCount;
+    }
+
+    isOver() {
+        return this.lives == 0;
+    }
+    
+    draw() {
+        this.drawScore();
+        this.drawLives();
+    }
+
+
+    addScore() {
+        this.score++;
+    }
+
+    die() {
+        this.lives--;
+    }
+
+}
+
+exports = GameInfo;
+
